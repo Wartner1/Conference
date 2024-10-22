81763f0aa1f85c35b2e29f571465c1d30f56e334
Wartner1 <deivid.sokol@gmail.com>
2024-10-22 23:04:54 +0300

    Student Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Routes for student
+Route::get('/', [StudentController::class, 'home'])->name('home');
+Route::post('student/register', [StudentController::class, 'register'])->name('student.register');
+Route::get('/profile', [StudentController::class, 'profile'])->name('student.profile');
+
+//Clients Routes
+Route::get('client/create', [ClientController::class, 'create'])->name('client.create'); // Show form to
+
+
+//Employee Routes
+Route::get('employee', [EmployeeController::class, 'index'])->name('employee.index'); // List all employees
+// Admin User Management Routes
+Route::get('admin/user', [UserController::class, 'index'])->name('admin.user.index'); // List all conferences
+
+//Admin Conferences Management Routes
+Route::get('conference', [ConferenceController::class, 'index'])->name('conference.index'); // List all conferences
+
